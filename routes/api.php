7b4ac0edfdca7871493ad8fd929bd06031f00cc5
Tomy rebot <tomas.rebot@adalid.net>
2021-08-08 05:49:59 +0200

    test 1

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/ 
+*/
 Route::prefix('v1')->group(function () {
+    
     Route::namespace('User')->group(function () {
         Route::post('register', 'UserController@register')->name('register');
     });
